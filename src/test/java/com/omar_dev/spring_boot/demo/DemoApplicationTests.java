6e492816a95710eb5100b9c369f64189e039c8c1
Omarmoatz <omar.raouf.002@gmail.com>
2025-02-27 16:33:24 +0200

    add project starter

---
+++ src/test/java/com/omar_dev/spring_boot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar_dev.spring_boot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
