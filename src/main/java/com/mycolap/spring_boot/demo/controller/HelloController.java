1ba82ce0a6fdda0d4ce424e2240229729356526c
Omarmoatz <omar.raouf.002@gmail.com>
2025-03-02 12:03:57 +0200

    tried dependency injection

--- src/main/java/com/mycolap/spring_boot/demo/controller/HelloController.java
+++ src/main/java/com/mycolap/spring_boot/demo/controller/HelloController.java
@@ -1,10 +1,22 @@
 package com.mycolap.spring_boot.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${owner.name}")
+    private String myName;
+
+    @Value("${owner.team}")
+    private String myTeam;
+
+    @GetMapping("/my-info")
+    public String myInfo(){
+        return "my name is: " + myName + ", my team is: " + myTeam;
+    }
     
     @GetMapping("/")
     public String helloWorld(){
